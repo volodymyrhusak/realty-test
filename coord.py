9ec8e68b5e4a42ec3acc1673001081fdeaa83047
volodymyrhusak <vovatrap@gmail.com>
2016-06-23 03:35:19 -0700

    except Exception:

--- coord.py
+++ coord.py
@@ -3,26 +3,36 @@ import MySQLdb
 
 
 def get_poligon_oblast():
-	db=MySQLdb.connect(host='localhost', user='root',passwd='123',db='orendatest')
-	dbCursor=db.cursor()
-	dbCursor.execute('''SELECT geojson FROM poligons WHERE idpoligons=39''')
-	poligons=dbCursor.fetchone()
-	print type(poligons)
-	return poligons
+	try:
+		db=MySQLdb.connect(host='localhost', user='root',passwd='123',db='orendatest')
+		dbCursor=db.cursor()
+		dbCursor.execute('''SELECT geojson FROM poligons WHERE idpoligons=39''')
+		poligons=dbCursor.fetchone()
+		print type(poligons)
+		return poligons
+	
+	except Exception:
+		print "MySQLdb error"
 
 
 
 def get_poligon_reg():
-	db=MySQLdb.connect(host='localhost', user='root',passwd='123',db='orendatest')
-	dbCursor=db.cursor()
-	dbCursor.execute('''SELECT geojson FROM poligons WHERE idpoligons=41''')
-	poligons=dbCursor.fetchone()
-	print type(poligons)
-	return poligons
+	try:
+		db=MySQLdb.connect(host='localhost', user='root',passwd='123',db='orendatest')
+		dbCursor=db.cursor()
+		dbCursor.execute('''SELECT geojson FROM poligons WHERE idpoligons=41''')
+		poligons=dbCursor.fetchone()
+		print type(poligons)
+		return poligons
+	except Exception:
+		print "MySQLdb error"
 
 def get_object():
-	db=MySQLdb.connect(host='localhost', user='root',passwd='123',db='orendatest')
-	dbCursor=db.cursor()
-	dbCursor.execute('''SELECT descr,coord FROM orendaPodobovo WHERE idorendaPodobovo <21 ''')
-	data=dbCursor.fetchall()
-	return data
+	try:
+		db=MySQLdb.connect(host='localhost', user='root',passwd='123',db='orendatest')
+		dbCursor=db.cursor()
+		dbCursor.execute('''SELECT descr,coord FROM orendaPodobovo WHERE idorendaPodobovo <21 ''')
+		data=dbCursor.fetchall()
+		return data
+	except Exception:
+		print "MySQLdb error"
