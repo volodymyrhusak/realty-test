d79f4b9ca60c2d0c2f8b56d28ea43d6f3e2cb742
volodymyrhusak <vovatrap@gmail.com>
2016-06-23 05:55:32 -0700

    poligons in file

--- app.py
+++ app.py
@@ -13,26 +13,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def mapview():
+
+	with open('oblPoligons.json','r') as f: 
+		poligons=json.dumps(f.read())
+
+	with open('regPoligons.json','r') as f: 
+		poligonsReg=json.dumps(f.read())
+	with open('pointPoligons.json','r') as f:
+		dictOdject=json.dumps(f.read())
     
 	# poligons=json.dumps(get_poligon_oblast()[0])
 	# poligonsReg=json.dumps(get_poligon_reg()[0])
- #    # with open('static/poligon.json','r') as f: 
- #    #     poligons=json.dumps(f.read())
- #    # print poligons
+    # with open('static/poligon.json','r') as f: 
+    #     poligons=json.dumps(f.read())
+    # print poligons
 	# listObject=get_object()
 	# print len(listObject)
 	# dictOdject=[]
 	# for x in listObject:
-	# 	descr=x[0]
-	# 	coord=x[1]
-	# 	coord=list(ast.literal_eval(coord))
-	# 	# dictOdject['descr']=descr
-	# 	dictOdject.append(coord)
-	# 	# print dictOdject
+		# descr=x[0]
+		# coord=x[1]
+		# coord=list(ast.literal_eval(coord))
+		# dictOdject['descr']=descr
+		# dictOdject.append(coord)
+		# print dictOdject
 	
 	# dictOdject=json.dumps(dictOdject)
+	# with open('pointPoligons.json','w') as f: 
+		# f.write(dictOdject)
 	 # poligons=poligons, poligonsReg=poligonsReg, dictOdject=dictOdject
-	return render_template('home.html')
+	return render_template('home.html',poligons=poligons, poligonsReg=poligonsReg, dictOdject=dictOdject)
 
 if __name__ == "__main__":
 	app.run(debug=True)
